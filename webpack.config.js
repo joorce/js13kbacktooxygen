26b440cff4b28a31f2432d00e7a9f5fed5623547
Jose Ortega <joseortega@joseortega.es>
2019-08-28 18:22:46 +0200

    Sound stuff

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+module.exports = {
+    entry: './main.js',
+    output: {
+        path: path.resolve(__dirname, ''),
+        filename: 'bundle.js'
+    },
+    mode: 'development',
+    devtool: 'source-map'
+};
